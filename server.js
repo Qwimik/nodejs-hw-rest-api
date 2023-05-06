92af84b782e696a48ddc59efc3ca6b1a7bae6de8
Oleksii Rozhnenko <qwimik@gmail.com>
2023-05-06 22:35:58 +0300

    DB_HOST replace of .env to server.js

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST, PORT = 3000 } = process.env;
+// const { DB_HOST, PORT = 3000 } = process.env;
+
+const DB_HOST =
+  "mongodb+srv://qwimik:qwe123@cluster0.kadyqc9.mongodb.net/contactsDB?retryWrites=true&w=majority";
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
+    app.listen(3000);
     console.log("Database connection successful");
   })
   .catch((error) => console.log(error.message));
