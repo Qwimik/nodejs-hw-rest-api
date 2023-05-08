410d24c58328b5c39cc3d41b00c4cdbab68240c1
Oleksii Rozhnenko <qwimik@gmail.com>
2023-05-09 00:10:54 +0300

    completed 04-auth

--- server.js
+++ server.js
@@ -2,17 +2,14 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-// const { DB_HOST, PORT = 3000 } = process.env;
-
-const DB_HOST =
-  "mongodb+srv://qwimik:qwe123@cluster0.kadyqc9.mongodb.net/contactsDB?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database connection successful");
   })
   .catch((error) => console.log(error.message));
